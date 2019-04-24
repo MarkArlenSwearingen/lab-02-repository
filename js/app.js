e7f6fa54b6efde0dee4664850dcd1d87a19ea079
Mark Swearingen <45861248+Swearingenius@users.noreply.github.com>
2019-04-24 08:33:48 -0700

    Template, Constructor Function and array

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+function Horn(horn){
+  this.image_url = horn.image_url;
+  this.title = horn.title;
+  this.description = horn.description;
+  this.keyword = horn.keyword;
+  this.horns = horn.horns;
+}
+
+Horn.allHorns = [];
+
+Horn.prototype.render = function(){
+
+}
+
